3b70214f495d161cb97f29dcc4827d6f138a5d09
kushvash <kushvash.usc@gmail.com>
2024-09-29 03:20:25 -0700

    Added game over scene

--- Assets/Scripts/plane_controller.cs
+++ Assets/Scripts/plane_controller.cs
@@ -1,36 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class plane_controller : MonoBehaviour
 {
-    // public float speed;
-    // public float rotationSpeed;
-    // public float verticalInput;
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     // get the user's vertical input
-    //     verticalInput = Input.GetAxis("Vertical");
-
-    //     // move the plane forward at a constant rate
-    //     transform.Translate(Vector3.right * speed * Time.deltaTime);
-
-    //     // tilt the plane up/down based on up/down arrow keys
-    //     transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput * -1);
-    // }
-
     public float speed;            // Speed of the plane
     public float tiltAmount;       // How much the plane tilts when changing height
     public float verticalInput;    // To capture W/S input
     public float maxTilt = 45f;    // Maximum angle to tilt
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collided with: " + collision.gameObject.name);
+
+        // Check if the plane collided with a wall
+        if (collision.gameObject.tag == "Wall")
+        {
+            Debug.Log("Collision with Wall detected!");
+
+            // Attempt to load the Game Over scene
+            Debug.Log("Loading Game Over Scene...");
+            SceneManager.LoadScene("GameOver");
+        }
+    }
 
     void Update()
     {
@@ -46,5 +39,6 @@ public class plane_controller : MonoBehaviour
         // Tilt the plane based on vertical input (rotate along the Z-axis for 2D tilt effect)
         float tilt = Mathf.Clamp(verticalInput * tiltAmount, -maxTilt, maxTilt); // Limit the tilt angle
         transform.rotation = Quaternion.Euler(0, 0, tilt);
+
     }
 }
