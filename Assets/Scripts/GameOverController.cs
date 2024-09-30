49554b1acbd462a9ca18b6001c07bfa74c0a3ad2
kushvash <kushvash.usc@gmail.com>
2024-09-30 00:18:34 -0700

    Start menu and controls screen added

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -11,8 +11,8 @@ public class GameOverController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Store the name of the previous scene (main game scene) before the GameOver scene is loaded
-        previousSceneName = PlayerPrefs.GetString("LastLoadedScene", "MainGameScene"); // Default to "MainGameScene"
+        // // Store the name of the previous scene (main game scene) before the GameOver scene is loaded
+        // previousSceneName = PlayerPrefs.GetString("LastLoadedScene", "MainGameScene"); // Default to "MainGameScene"
     }
 
     // Update is called once per frame
@@ -23,12 +23,32 @@ public class GameOverController : MonoBehaviour
         {
             RestartGame(); // Call the restart function
         }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            MainMenu(); // Call the restart function
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Controls(); // Call the restart function
+        }
     }
 
     // Function to reload the previous scene
     public void RestartGame()
     {
-        SceneManager.LoadScene(previousSceneName);
+        SceneManager.LoadScene("FromClass");
     }
 
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene("ControlsMenu");
+    }
+    
 }
---
+++ Assets/Scripts/StartMenuScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuScript : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Detect any key press or mouse click
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartGame(); // Call the restart function
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Controls(); // Call the restart function
+        }
+
+    }
+
+    // Function to reload the previous scene
+    public void StartGame()
+    {
+        SceneManager.LoadScene("FromClass");
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene("ControlsMenu");
+    }
+}
